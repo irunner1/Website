8ddc292f71f6204eb156e394f9d1e5a22e802733
irunner1 <aspuchkov02@gmail.com>
2021-12-05 00:14:19 +0300

    added website

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('.header_burger').click(function(event){
+        $('.header_burger,.header_menu,.outline').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+});
+
+$('#orderDefine li a').on('click', function() {
+		
+    var order = $(this).prop('rel');
+    
+    $('#orderDefine li a.active').removeClass('active');
+    $(this).addClass('active');
+
+    if ($(this).hasClass('active')) {
+        $('.latest').removeClass('active');
+        $('.popular').removeClass('active');
+        $('.throwbacks').removeClass('active');
+    }
+    if (order == 'all') {
+        $('.latest').toggleClass('active');
+        $('.popular').removeClass('active');
+        $('.throwbacks').removeClass('active');
+    }
+    if (order == 'popular') {
+        $('.popular').toggleClass('active');
+        $('.latest').removeClass('active');
+        $('.throwbacks').removeClass('active');
+    }
+    if (order == 'throwback') {
+        $('.throwbacks').toggleClass('active');
+        $('.latest').removeClass('active');
+        $('.popular').removeClass('active');
+    } 
+});
